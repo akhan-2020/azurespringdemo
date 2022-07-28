261a8c77564311b28994de3c102d604360423b91
Adnan Khan <adnan.x.khan@gmail.com>
2022-07-28 16:15:55 -0500

    updates

--- src/main/java/com/example/azurespring/azurespringdemo/HelloController.java
+++ src/main/java/com/example/azurespring/azurespringdemo/HelloController.java
@@ -1,9 +1,14 @@
 package com.example.azurespring.azurespringdemo;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.http.MediaType;
 import org.springframework.util.Assert;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -15,6 +20,16 @@ public class HelloController {
 
     @Autowired
     private ItemsRepository repository;
+
+    @PostMapping("/item")
+    Mono<Item> newEmployee(@RequestBody Item item) {
+        return repository.save(item);
+    }
+
+    @GetMapping(path = "/item/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    Mono<Item> one(@PathVariable Long id) {
+        return repository.findById(id.toString());    
+    }
 
     @RequestMapping("/")
     public String index() {
